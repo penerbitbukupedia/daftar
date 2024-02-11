3876e94316e65860a3543874394adf87291ebb1f
Rolly Maulana Awangga <awangga@gmail.com>
2024-02-11 11:45:16 +0700

    sip

--- js/main.js
+++ js/main.js
@@ -1,20 +1,25 @@
 import {getCookie} from "https://cdn.jsdelivr.net/gh/jscroot/cookie@0.0.1/croot.js";
 import {redirect} from "https://cdn.jsdelivr.net/gh/jscroot/url@0.0.9/croot.js";
-import {onClick,getValue} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
+import {onClick,getValue,setValue} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
 import { postWithToken } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.6/croot.js";
 
-//check if login cookies is exist
-if(!getCookie("login")){
-    redirect("/");
+getWithHeader("https://ped.fly.dev/auth/userdata","login",getCookie("login"),tokenFunction);
+function tokenFunction(result){
+    if(!result){
+        redirect("/");
+    }else{
+        console.log(result.phone);
+        setValue("phone",result.phone);
+        onClick("submit",PostSignUp);
+    }
 }
 
-onClick("submit",PostSignUp);
+
 
 function PostSignUp(){
     let datainjson = {
         "nama": getValue("nama"),
         "email": getValue("email"),
-        "phone":getValue("phone"),
         "nik":getValue("nik"),
         "pekerjaan":getValue("pekerjaan"),
         "alamat":getValue("alamat")
