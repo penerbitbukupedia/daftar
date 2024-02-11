0a3a57ad8266275b60ee525ea35eabbed3e324df
Rolly Maulana Awangga <awangga@gmail.com>
2024-02-11 15:51:27 +0700

    getWithHeader

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import {getCookie} from "https://cdn.jsdelivr.net/gh/jscroot/cookie@0.0.1/croot.js";
 import {redirect} from "https://cdn.jsdelivr.net/gh/jscroot/url@0.0.9/croot.js";
 import {onClick,getValue,setValue} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
-import { postWithToken } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.6/croot.js";
+import { postWithToken,getWithHeader } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.6/croot.js";
 
 getWithHeader("https://ped.fly.dev/auth/userdata","login",getCookie("login"),tokenFunction);
 function tokenFunction(result){
